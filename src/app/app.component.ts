5e3d742d69190b0883b6401e30c2a58c77e49598
migue <35717991+mrivasperez@users.noreply.github.com>
2025-01-08 10:22:08 -0800

    Base UI (#1)
    * task: install angular material
    
    * task: add trivia categories
    
    * refactor: retitle data to options and include all trivia question options
    
    * ui: select category option
    
    * style: number of questions slider
    
    * ui: difficulty selection
    
    * ui: question type selection
    
    * refactor: include id for category type of any
    
    * task: scaffold open trivia service
    
    * task: prototype submit event handler
    
    * task: handle select category
    
    * task: handle select difficulty, question type
    
    * task: handle selecting number of questions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { CATEGORIES, DIFFICULTY, QUESTION_TYPES } from '../data/options';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    MatSliderModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'trivial';
+  categoryOptions = CATEGORIES;
+  difficultyOptions = DIFFICULTY;
+  questionTypeOptions = QUESTION_TYPES;
+  numberOfQuestions: number = 5;
+
+  selectedCategory: number = 0;
+  selectedDifficulty: string = '';
+  selectedQuestionType: string = '';
+
+  onSelectCategory(event: MatSelectChange) {
+    this.selectedCategory = event.value;
+  }
+
+  onSelectDifficulty(event: MatSelectChange) {
+    this.selectedDifficulty = event.value;
+  }
+
+  onSelectQuestionType(event: MatSelectChange) {
+    this.selectedQuestionType = event.value;
+  }
+
+  onSelectNumberOfQuestions(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.numberOfQuestions = Number(input.value);
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    alert('Form Submitted');
+
+    console.log(this.selectedCategory);
+    console.log(this.selectedDifficulty);
+    console.log(this.selectedQuestionType);
+    console.log(this.numberOfQuestions);
+  }
 }
