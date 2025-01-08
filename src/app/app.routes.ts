650b80b64d3bf90e0672bcde70c63d36fd830c49
migue <35717991+mrivasperez@users.noreply.github.com>
2025-01-08 11:00:03 -0800

    task: scaffold game options component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { GameComponent } from './game/game.component';
+import { GameOptionsComponent } from './game-options/game-options.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: GameOptionsComponent },
+  { path: 'game', component: GameComponent },
+];
---
+++ src/app/game-options/game-options.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-options',
+  imports: [],
+  templateUrl: './game-options.component.html',
+  styleUrl: './game-options.component.css'
+})
+export class GameOptionsComponent {
+
+}
