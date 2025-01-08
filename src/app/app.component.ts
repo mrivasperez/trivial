6e4a083bc938ec1ea289e7187b871b58b19d1b83
migue <35717991+mrivasperez@users.noreply.github.com>
2025-01-08 08:16:48 -0800

    style: number of questions slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,17 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
 import { CATEGORIES, DIFFICULTY, QUESTION_TYPES } from '../data/options';
 @Component({
   selector: 'app-root',
-  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    MatSliderModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
