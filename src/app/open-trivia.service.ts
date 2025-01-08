7d5312c4a7e4114140a452f65720ddb242faf50b
migue <35717991+mrivasperez@users.noreply.github.com>
2025-01-08 10:31:42 -0800

    task: create types and base url for open trivia api request

--- src/app/open-trivia.service.ts
+++ src/app/open-trivia.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+interface OpenTriviaQuestion {
+  type: 'boolean' | 'multiple';
+  difficulty: 'easy' | 'medium' | 'hard';
+  category: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+}
+
+interface OpenTriviaResponse {
+  response_code: number;
+  results: OpenTriviaQuestion[];
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OpenTriviaService {
-
-  constructor() { }
+  private apiUrl = 'https://opentdb.com/api.php';
+  constructor(private http: HttpClient) {}
 }
