d6be28f3c779ff1ee599cd230ff03f0e1c8b35bd
migue <35717991+mrivasperez@users.noreply.github.com>
2025-01-08 11:10:29 -0800

    task: load params for game and fetch API for questions

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OpenTriviaQuestion, OpenTriviaService } from '../open-trivia.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observer } from 'rxjs';
 
 @Component({
   selector: 'app-game',
   imports: [],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.css'
+  styleUrl: './game.component.css',
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  questions: OpenTriviaQuestion[] = [];
+  loading: boolean = true;
+  error: string | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private openTriviaService: OpenTriviaService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      const amount = Number(params['amount'] || 10);
+      const category = Number(params['category'] || 0);
+      const difficulty = params['difficulty'] || 0;
+      const type = params['type'] || 0;
+
+      this.loading = true;
+      this.error = null;
+
+      this.loading = true;
+      this.error = null;
+
+      const observer: Observer<any> = {
+        next: (response) => {
+          this.questions = response.results;
+          this.loading = false;
+          console.log(response)
+        },
+        error: (error) => {
+          this.error = 'Failed to load trivia questions.';
+          this.loading = false;
+          console.error('API Error:', error);
+        },
+        complete: () => {},
+      };
+
+      this.openTriviaService
+        .getTriviaQuestions(amount, category, difficulty, type)
+        .subscribe(observer);
+    });
+  }
 }
