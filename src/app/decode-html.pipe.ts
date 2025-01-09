18f2678ffb13bbb1ff7d158e629680bf9f5a3042
Miguel Rivas Perez <mrivasperez@icloud.com>
2025-01-08 16:10:46 -0800

    fix: create DocodeHtmlPipe to display questions without html characters

--- src/app/decode-html.pipe.ts
+++ src/app/decode-html.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Pipe({
-  name: 'decodeHtml'
+  name: 'decodeHtml',
 })
 export class DecodeHtmlPipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) {}
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): any {
+    const doc = new DOMParser().parseFromString(value, 'text/html');
+    return doc.documentElement.textContent;
   }
-
 }
